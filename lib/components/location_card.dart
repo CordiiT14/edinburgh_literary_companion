7fbe714f54489c77d4c5e3cf2b3d03df7f61b582
Dan7-7-7 <dannyranson19@gmail.com>
2022-05-31 16:18:33 +0100

    minor edit

---
+++ lib/components/location_card.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:edin_lit_companion/models/Location.dart';
+
+class LocationCard extends StatelessWidget {
+  final Location location;
+  final Function delete;
+
+  LocationCard({required this.location, required this.delete});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsetsDirectional.fromSTEB(16.0, 16.0, 16.0, 0),
+      child: ListTile(
+        onTap: () {
+          //  re-route to the location page
+        },
+        title: Text(
+          location.name,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.blueGrey[200],
+          ),
+        ),
+        subtitle: Text(
+          //this is to extract 'landmark' from 'Category.landmark'
+          location.category.toString().split('.').last,
+          style: TextStyle(
+            fontSize: 12.0,
+            color: Colors.blueGrey[150],
+          ),
+        ),
+        trailing: IconButton(
+          icon: Icon(
+            Icons.delete,
+            semanticLabel: 'remove from saved',
+          ),
+          tooltip: 'remove from saved',
+          onPressed: () {
+            delete();
+          },
+        ),
+      ),
+    );
+  }
+}
