7aa94e11416ab61ce267dd94f451c5c4dd05dcdb
Dan7-7-7 <dannyranson19@gmail.com>
2022-06-01 11:17:44 +0100

    adds comments

--- lib/components/location_list_card.dart
+++ lib/components/location_list_card.dart
@@ -5,9 +5,8 @@ import 'package:edin_lit_companion/pages/location_view.dart';
 // Card widget for locations list on Discover (browse.dart) screen using data list mapped from browse.dart
 
 class LocationListCard extends StatefulWidget {
-  // const LocationListCard({Key? key}) : super(key: key); NOT NEEDED AS YET
+  const LocationListCard({Key? key, required this.location}) : super(key: key);
 
-  const LocationListCard({required this.location});
   final Location location;
 
   @override
@@ -17,53 +16,56 @@ class LocationListCard extends StatefulWidget {
 class _LocationListCardState extends State<LocationListCard> {
   @override
   Widget build(BuildContext context) {
-      return ListTile(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
+    return ListTile(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
               builder: (context) => LocationView(location: widget.location)
               // TODO: Use the line below
               // builder: (context) => LocationView(location: location,)
               ),
-          );
+        );
+      },
+      // Location image
+      leading: CircleAvatar(
+        backgroundImage: AssetImage('assets/${widget.location.image}'),
+        radius: 20.0,
+      ),
+      // Location name
+      title: Text(widget.location.name,
+          style: const TextStyle(
+            fontSize: 18.0,
+          )),
+
+      // Address or shortened description
+      // subtitle: Text(location.address,
+      //     style: const TextStyle(
+      //       fontSize: 12.0,
+      //     )),
+
+      // Saved icon
+      trailing: TextButton.icon(
+        onPressed: () {
+          setState(() {
+            //updating saved boolean in location
+            widget.location.saved = !widget.location.saved;
+          });
         },
-        // Location image
-        leading: CircleAvatar(
-          backgroundImage: AssetImage('assets/${widget.location.image}'),
-          radius: 20.0,
+        label: const Text(''),
+        icon: Icon(
+          //changing icon display based on saved status
+          widget.location.saved
+              ? Icons.favorite
+              : Icons.favorite_border_outlined,
+          color: widget.location.saved ? Colors.red : Colors.grey[500],
+          semanticLabel: widget.location.saved ? 'Remove from saved' : 'Save',
         ),
-        // Location name
-        title: Text(widget.location.name,
-            style: const TextStyle(
-              fontSize: 18.0,
-            )),
+      ),
 
-        // Address or shortened description
-        // subtitle: Text(location.address,
-        //     style: const TextStyle(
-        //       fontSize: 12.0,
-        //     )),
-
-        // Saved icon
-        trailing: TextButton.icon(
-          onPressed: () {
-            setState((){
-              widget.location.saved = !widget.location.saved;
-            });
-            },
-          label: const Text(''),
-          icon: Icon(
-              widget.location.saved ? Icons.favorite : Icons.favorite_border_outlined,
-            color: widget.location.saved ? Colors.red : Colors.grey[500],
-            semanticLabel: widget.location.saved ? 'Remove from saved' : 'Save',
-          ),
-        ),
-
-        // If we want slightly larger cards, could do ThreeLine
-        // isThreeLine: true,
-
-      );
+      // If we want slightly larger cards, could do ThreeLine
+      // isThreeLine: true,
+    );
     // );
   }
 }
