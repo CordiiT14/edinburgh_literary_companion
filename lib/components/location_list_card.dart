9b657e028cde1523b6984acca8f08d7ac6a85576
CLg003 <clairemjlaing@gmail.com>
2022-06-01 09:50:34 +0100

    Added comments to Discover screen files.

--- lib/components/location_list_card.dart
+++ lib/components/location_list_card.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:edin_lit_companion/models/Location.dart';
 import 'package:edin_lit_companion/pages/location_view.dart';
 
+// Card widget for locations list on Discover (browse.dart) screen using data list mapped from browse.dart
+
 class LocationListCard extends StatelessWidget {
-  // const LocationListCard({Key? key}) : super(key: key);
+  // const LocationListCard({Key? key}) : super(key: key); NOT NEEDED AS YET
 
   const LocationListCard({required this.location});
   final Location location;
 
   @override
   Widget build(BuildContext context) {
-    // return Card(
       return ListTile(
         onTap: () {
           Navigator.push(
@@ -22,18 +23,24 @@ class LocationListCard extends StatelessWidget {
               ),
           );
         },
+        // Location image
         leading: CircleAvatar(
-          backgroundImage: AssetImage('assets/monuments/${location.image}'),
+          backgroundImage: AssetImage('assets/${location.image}'),
           radius: 20.0,
         ),
+        // Location name
         title: Text(location.name,
             style: const TextStyle(
               fontSize: 18.0,
             )),
+
+        // Address or shortened description
         // subtitle: Text(location.address,
         //     style: const TextStyle(
         //       fontSize: 12.0,
         //     )),
+
+        // Saved icon
         trailing: TextButton.icon(
           onPressed: () {},
           label: const Text(''),
@@ -42,7 +49,10 @@ class LocationListCard extends StatelessWidget {
             color: Colors.grey[500],
           ),
         ),
+
+        // If we want slightly larger cards, could do ThreeLine
         // isThreeLine: true,
+
       );
     // );
   }
