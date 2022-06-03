853628f1e296781fb2c46ded373b229796a6e0bc
CLg003 <clairemjlaing@gmail.com>
2022-06-03 17:55:08 +0100

    Working on expandable panel for book list cards.

--- lib/components/book_list_card.dart
+++ lib/components/book_list_card.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:edin_lit_companion/models/Book.dart';
+import 'package:edin_lit_companion/pages/book_detail.dart';
+import 'package:edin_lit_companion/providers/books_provider.dart';
+import 'package:provider/provider.dart';
+
+import 'package:expandable/expandable.dart';
 
 // CARD WIDGET FOR BOOKS list on Books screen using data passed from books.dart
 
 class BookDetail extends StatelessWidget {
   const BookDetail({Key? key}) : super(key: key);
+  final Book book;
+  // BookDetail(this.book);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('book detail here'),
+    return ExpandablePanel(
+        header: Text(book.title),
+        collapsed: Text(book.author),
+        expanded: Column(
+          children: [
+            Text(book.author),
+            Text(book.description, softWrap: true),
+            Image.asset(book.image),
+          ],
+        ),
     );
   }
 }
