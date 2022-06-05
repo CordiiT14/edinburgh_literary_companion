38b01cfab3c0d35b035908cc970245cff1706f9c
Kevin Muldoon <100124261+kevinmuldoon-github@users.noreply.github.com>
2022-06-05 11:03:20 +0100

    Added info area with how to use and meet the team tabs

---
+++ lib/pages/info.dart
@@ -0,0 +1,382 @@
+import 'package:flutter/material.dart';
+
+import '../components/navigationBar.dart';
+
+class Info extends StatelessWidget {
+  const Info({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Information'),
+          centerTitle: true,
+          backgroundColor: const Color.fromRGBO(87, 88, 187, 9.0),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Info()),
+                  );
+                },
+                icon: const Icon(Icons.info)),
+          ],
+          bottom: const TabBar(
+            indicatorColor: Color.fromRGBO(241, 135, 1, 1),
+            tabs: [
+              Tab(
+                icon: Icon(Icons.apps),
+                text: 'How to Use the App',
+              ),
+              Tab(
+                icon: Icon(Icons.people),
+                text: 'Meet the Team',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            ListView(
+              // Contents of first tab
+              children: [
+                const ListTile(
+                  title: Text(
+                    'How to Use the App',
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Edinburgh Literary Companion',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+
+                // Home summary
+                const SizedBox(height:20),
+                  Row(
+                    children: [
+                      const SizedBox(width:20),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: const [
+                          Icon(
+                              Icons.home_outlined,
+                              color: Color.fromRGBO(241, 135, 1, 1),
+                              size: 50,
+                          ),
+                          Text('Home'),
+                        ],
+                      ),
+                      const SizedBox(width:20),
+                      Flexible(
+                        child: Column(
+                          children: const [
+                            Text(
+                              "Our home page lists Edinburgh's top attractions, landmarks and bookshops.",
+                              style: TextStyle(
+                                fontSize: 17,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width:20),
+                    ],
+                  ),
+
+                // Discover summary
+                const SizedBox(height:20),
+                Row(
+                  children: [
+                    const SizedBox(width:20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const [
+                        Icon(
+                          Icons.explore_outlined,
+                          color: Color.fromRGBO(241, 135, 1, 1),
+                          size: 50,
+                        ),
+                        Text('Discover'),
+                      ],
+                    ),
+                    const SizedBox(width:20),
+                    Flexible(
+                      child: Column(
+                        children: const [
+                          Text(
+                            "A detailed overview of all attractions, landmarks and book shops.",
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width:20),
+                  ],
+                ),
+
+                // Map summary
+                const SizedBox(height:20),
+                Row(
+                  children: [
+                    const SizedBox(width:20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const [
+                        Icon(
+                          Icons.location_on_outlined,
+                          color: Color.fromRGBO(241, 135, 1, 1),
+                          size: 50,
+                        ),
+                        Text('Map'),
+                      ],
+                    ),
+                    const SizedBox(width:20),
+                    Flexible(
+                      child: Column(
+                        children: const [
+                          Text(
+                            "All locations can be viewed on our detailed map of Edinburgh.",
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width:20),
+                  ],
+                ),
+
+                // Books summary
+                const SizedBox(height:20),
+                Row(
+                  children: [
+                    const SizedBox(width:20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const [
+                        Icon(
+                          Icons.menu_book_rounded,
+                          color: Color.fromRGBO(241, 135, 1, 1),
+                          size: 50,
+                        ),
+                        Text('Books'),
+                      ],
+                    ),
+                    const SizedBox(width:20),
+                    Flexible(
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Discover books where the city of Edinburgh is part of the story.",
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width:20),
+                  ],
+                ),
+
+                // Saved summary
+                const SizedBox(height:20),
+                Row(
+                  children: [
+                    const SizedBox(width:20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const [
+                        Icon(
+                          Icons.favorite_border_outlined,
+                          color: Color.fromRGBO(241, 135, 1, 1),
+                          size: 50,
+                        ),
+                        Text('Saved'),
+                      ],
+                    ),
+                    const SizedBox(width:20),
+                    Flexible(
+                      child: Column(
+                        children: const [
+                          Text(
+                            "A list of all locations you have saved.",
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width:20),
+                  ],
+                ),
+              // End of navigation menu summaries
+              ],
+            ),
+            ListView(
+              // Contents of second tab
+              children: [
+                const ListTile(
+                  title: Text(
+                    'Meet the Team',
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Edinburgh Literary Companion',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+
+                // Team Member: Claire
+                const SizedBox(height:20),
+                Row(
+                  children: [
+                    const SizedBox(width:20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset(
+                          'assets/team/claire.jpeg', //
+                          width: 100,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width:20),
+                    Flexible(
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Claire Laing",
+                            style: TextStyle(
+                              fontSize: 30,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width:20),
+                  ],
+                ),
+
+                // Team Member: Daniel
+                const SizedBox(height:20),
+                Row(
+                  children: [
+                    const SizedBox(width:20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset(
+                          'assets/team/daniel.jpeg', //
+                          width: 100,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width:20),
+                    Flexible(
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Daniel Ranson",
+                            style: TextStyle(
+                              fontSize: 30,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width:20),
+                  ],
+                ),
+
+                // Team Member: Cordii
+                const SizedBox(height:20),
+                Row(
+                  children: [
+                    const SizedBox(width:20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset(
+                          'assets/team/cordii.jpeg', //
+                          width: 100,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width:20),
+                    Flexible(
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Cordelia T. Johnstone",
+                            style: TextStyle(
+                              fontSize: 30,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width:20),
+                  ],
+                ),
+
+                // Team Member: Kevin
+                const SizedBox(height:20),
+                Row(
+                  children: [
+                    const SizedBox(width:20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset(
+                          'assets/team/kevin.jpeg', //
+                          width: 100,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width:20),
+                    Flexible(
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Kevin Muldoon",
+                            style: TextStyle(
+                              fontSize: 30,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width:20),
+                  ],
+                ),
+
+
+
+              ],
+            ),
+          ],
+        ),
+        bottomNavigationBar: const Navigation(
+          selectedIndex: 2,
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/view_map.dart
+++ lib/pages/view_map.dart
@@ -5,6 +5,8 @@ import 'package:edin_lit_companion/data/location_data.dart';
 import 'package:edin_lit_companion/models/Location.dart';
 import 'package:edin_lit_companion/pages/location_view.dart';
 
+import 'info.dart';
+
 class ViewMap extends StatefulWidget {
   const ViewMap({Key? key}) : super(key: key);
 
@@ -13,32 +15,31 @@ class ViewMap extends StatefulWidget {
 }
 
 class _ViewMapState extends State<ViewMap> {
-
   double initialLatitude = 55.94959278;
   double initialLongitude = -3.19338131;
+  final Map<String, Marker> _markers = {};
 
   // late GoogleMapController mapController;
   //  mapController = controller;
   // final LatLng _center = const LatLng(55.94936682425343, -3.1999283672172485);
 
-  final Map<String, Marker> _markers = {};
-
   void _onMapCreated(GoogleMapController controller) {
-
     //creating the pin locations
-    final List<Location> locations = LocationData().locations;
+    List<Location> locations = LocationData().locations;
+    // final List<Location> locations = context.watch<Locations>().allLocations;
 
     setState(() {
       _markers.clear();
-      for (var location in locations){
-
+      for (var location in locations) {
         // Determine colour of icon based on location type
-        BitmapDescriptor iconColour () {
-          if (location.category.index == 0) { // Is location an attraction?
+        BitmapDescriptor iconColour() {
+          if (location.category.index == 0) {
+            // Is location an attraction?
             return BitmapDescriptor.defaultMarkerWithHue(
                 BitmapDescriptor.hueBlue);
           }
-          if (location.category.index == 1) { // Is location a landmark?
+          if (location.category.index == 1) {
+            // Is location a landmark?
             return BitmapDescriptor.defaultMarkerWithHue(
                 BitmapDescriptor.hueViolet);
           }
@@ -53,27 +54,41 @@ class _ViewMapState extends State<ViewMap> {
           infoWindow: InfoWindow(
             title: location.name,
             snippet: location.address, // Breakdown of snippet is above
-            onTap: (){
+            onTap: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                    builder: (context) => LocationView(location: location)
-                ),
+                    builder: (context) => LocationView(location: location)),
               );
             },
           ),
         );
         _markers[location.name] = marker;
-
       } // end for loop
     });
   }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text('Location Map'),
+        centerTitle: true,
         backgroundColor: const Color.fromRGBO(87, 88, 187, 9.0),
+        actions:[
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const Info()
+              ),
+            );
+            },
+              icon: const Icon(Icons.info)
+          )
+        ]
       ),
       body: GoogleMap(
           onMapCreated: _onMapCreated,
@@ -87,3 +102,51 @@ class _ViewMapState extends State<ViewMap> {
     );
   }
 }
+
+// Tabbed map setup
+//   @override
+//   Widget build(BuildContext context) {
+//     int tabIndex=0 ;
+//         // DefaultTabController.of(context)?.index;
+//     return DefaultTabController(
+//       length: 5,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Location Map'),
+//           centerTitle: true,
+//           backgroundColor: const Color.fromRGBO(87, 88, 187, 9.0),
+//           bottom: const TabBar(
+//             indicatorColor: Color.fromRGBO(241, 135, 1, 1),
+//             tabs: [
+//               Tab(icon: Icon(Icons.tab)),
+//               Tab(icon: Icon(Icons.attractions)),
+//               Tab(icon: Icon(Icons.location_city)),
+//               Tab(icon: Icon(Icons.library_books)),
+//               Tab(icon: Icon(Icons.favorite)),
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: [
+//             GoogleMap(
+//                 onMapCreated: _onMapCreated,
+//                 initialCameraPosition: CameraPosition(
+//                   target: LatLng(initialLatitude, initialLongitude),
+//                   zoom: 11.0,
+//                 ),
+//                 markers: _markers.values.toSet()
+//             ),
+//             Text('${DefaultTabController.of(context)?.index}'),
+//             Text('$tabIndex'),
+//             Text('$tabIndex'),
+//             Text('$tabIndex'),
+//           ],
+//         ),
+//         bottomNavigationBar: const Navigation(
+//           selectedIndex: 2,
+//         ),
+//       ),
+//     );
+//   }
+// }
+
